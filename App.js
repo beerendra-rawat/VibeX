aad4e88b994cf99708b68be60275578f833fe85f
beerendra-rawat <rawatbeerendra2001.com>
2026-02-25 10:19:37 +0530

    update stack navigator

--- App.js
+++ App.js
@@ -1,62 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import BottomTab from './scr/navigations/BottomTab';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { AuthProvider, AuthContext } from './scr/context/AuthContext';
-import { FavoriteProvider } from './scr/context/FavoriteContext';
-import MusicScreen from './scr/screens/MusicScreen';
-import GoogleAuthScreen from './scr/screens/GoogleAuthScreen';
-import PrivacyScreen from './scr/screens/PrivacyScreen';
-import HelpAndSupportScreen from './scr/screens/HelpAndSupportScreen';
-import AboutAppScreen from './scr/screens/AboutAppScreen';
-import AccountScreen from './scr/screens/AccountScreen';
-import SearchScreen from './scr/screens/SearchScreen';
-import { MusicProvider } from './scr/context/MusicContext';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import { useContext, useEffect } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { useEffect } from "react";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
-const Stack = createNativeStackNavigator();
+import { AuthProvider } from "./scr/context/AuthContext";
+import { FavoriteProvider } from "./scr/context/FavoriteContext";
+import { MusicProvider } from "./scr/context/MusicContext";
 
-function AppNavigator() {
-  const { user, loading } = useContext(AuthContext);
-
-  if (loading) return null;
-
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {user ? (
-        <>
-          <Stack.Screen name="Main" component={BottomTab} />
-          <Stack.Screen name='MusicScreen' component={MusicScreen} />
-          <Stack.Screen name="Account" component={AccountScreen} />
-          <Stack.Screen name="Privacy" component={PrivacyScreen} />
-          <Stack.Screen name="HelpAndSupport" component={HelpAndSupportScreen} />
-          <Stack.Screen name="AboutApp" component={AboutAppScreen} />
-          <Stack.Screen name="SearchScreen" component={SearchScreen} />
-        </>
-      ) : (
-        <Stack.Screen name='GoogleAuthScreen' component={GoogleAuthScreen} />
-      )}
-    </Stack.Navigator>
-  );
-}
+import StackNavigator from "./scr/navigations/StackNavigator";
 
 export default function App() {
 
   useEffect(() => {
     GoogleSignin.configure({
-      "webClientId": "349500459690-id6rht9d5a1hodim2pvrpch4ipqng644.apps.googleusercontent.com",
+      webClientId: "349500459690-id6rht9d5a1hodim2pvrpch4ipqng644.apps.googleusercontent.com",
       scopes: ["profile", "email"],
       offlineAccess: true,
     });
-  }, [])
+  }, []);
+
   return (
     <AuthProvider>
       <MusicProvider>
         <FavoriteProvider>
           <NavigationContainer>
-            <StatusBar style='light' />
-            <AppNavigator />
+            <StatusBar style="light" />
+            <StackNavigator />
           </NavigationContainer>
         </FavoriteProvider>
       </MusicProvider>
---
+++ scr/navigations/StackNavigator.jsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthContext } from "../context/AuthContext";
+
+import BottomTab from "./BottomTab";
+import MusicScreen from "../screens/MusicScreen";
+import GoogleAuthScreen from "../screens/GoogleAuthScreen";
+import PrivacyScreen from "../screens/PrivacyScreen";
+import HelpAndSupportScreen from "../screens/HelpAndSupportScreen";
+import AboutAppScreen from "../screens/AboutAppScreen";
+import AccountScreen from "../screens/AccountScreen";
+import SearchScreen from "../screens/SearchScreen";
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+    const { user, loading } = useContext(AuthContext);
+
+    if (loading) return null;
+
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            {user ? (
+                <>
+                    <Stack.Screen name="Main" component={BottomTab} />
+                    <Stack.Screen name="MusicScreen" component={MusicScreen} />
+                    <Stack.Screen name="Account" component={AccountScreen} />
+                    <Stack.Screen name="Privacy" component={PrivacyScreen} />
+                    <Stack.Screen name="HelpAndSupport" component={HelpAndSupportScreen} />
+                    <Stack.Screen name="AboutApp" component={AboutAppScreen} />
+                    <Stack.Screen name="SearchScreen" component={SearchScreen} />
+                </>
+            ) : (
+                <Stack.Screen name="GoogleAuthScreen" component={GoogleAuthScreen} />
+            )}
+        </Stack.Navigator>
+    );
+};
+
+export default StackNavigator;
